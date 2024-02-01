03d9c8fb0a5a563a87ab40f27d5adcbb01fe77a9
Mahmoud Mohamed <matterm74m7@gmail.com>
2024-02-01 04:13:50 +0200

    Update switch_OnTypes.cs

--- Learning/C#/y_advanced topics/switch_OnTypes.cs
+++ Learning/C#/y_advanced topics/switch_OnTypes.cs
@@ -36,6 +36,57 @@ namespace Revision
                     break;
             }
 
+
+            // Checking Range Example:
+            int number = 15;
+            switch (number)
+            {
+                case int n when n > 10 && n <= 20:
+                    Console.WriteLine("Number is between 11 and 20.");
+                    break;
+                case int n when n > 20:
+                    Console.WriteLine("Number is greater than 20.");
+                    break;
+                default:
+                    Console.WriteLine("Number is 10 or less.");
+                    break;
+            }
+
+            // Checking Type and Property Example:
+            object obj = "Hello";
+            switch (obj)
+            {
+                case string s when s.Length > 5:
+                    Console.WriteLine("String has more than 5 characters.");
+                    break;
+                case string s when s.Length <= 5:
+                    Console.WriteLine("String has 5 or fewer characters.");
+                    break;
+                case int i:
+                    Console.WriteLine("It's an integer.");
+                    break;
+                default:
+                    Console.WriteLine("Unknown type.");
+                    break;
+            }
+
+
+            // Checking for Null Example:
+            string name = null;
+            switch (name)
+            {
+                case string s when !string.IsNullOrEmpty(s):
+                    Console.WriteLine("Name is not null or empty.");
+                    break;
+                case null:
+                    Console.WriteLine("Name is null.");
+                    break;
+                default:
+                    Console.WriteLine("Name is empty.");
+                    break;
+            }
+
+
         }
     }
 }
