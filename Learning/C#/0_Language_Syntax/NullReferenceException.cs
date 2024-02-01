2d432d1a71db2a88b2e6ce0d3747856853fadba0
Mahmoud Mohamed <matterm74m7@gmail.com>
2024-02-01 03:21:00 +0200

    Update NullReferenceException.cs

--- Learning/C#/0_Language_Syntax/NullReferenceException.cs
+++ Learning/C#/0_Language_Syntax/NullReferenceException.cs
@@ -17,9 +17,15 @@ namespace Revision
             // انت بتحاول تنفذ حاجة علي شيء مش موجود اصلا
 
 
-            // Explanation: You are trying to access a method (ToLower()) on a string variable (str),
+            // You are trying to access a method (ToLower()) on a string variable (str),
             // but the variable is currently set to null. Null represents the absence of an object reference,
             // so attempting to perform an operation on it will lead to a NullReferenceException.
+
+            // We can prevent the exception using the 'Null Conditional Operator' (?)
+            var str3 = str?.ToLower();
+            // By adding ? after the variable name, we are saying if it's not null, call the ToLower() method;
+            // otherwise, return null. This prevents a NullReferenceException and assigns null to str3 if str is null.
+            
         }
     }
 }
