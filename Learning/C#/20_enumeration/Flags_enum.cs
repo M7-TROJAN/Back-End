6ee60e29f951577f2fb08c5e424c9e6c0bb499d2
Mahmoud Mohamed <matterm74m7@gmail.com>
2024-02-12 21:21:23 +0200

    Update Flags_enum.cs

---
+++ Learning/C#/20_enumeration/Flags_enum.cs
@@ -0,0 +1,12 @@
+/*
+In C#, the [Flags] attribute is used to indicate that an enum is intended to be used as bit flags, where each value represents a single bit. 
+This attribute provides additional information to the compiler and developers, indicating that certain operations such as bitwise AND, OR, XOR, 
+and NOT can be applied to the enum values. 
+*/
+
+using System;
+
+namespace Enumerations
+{
+  
+}
