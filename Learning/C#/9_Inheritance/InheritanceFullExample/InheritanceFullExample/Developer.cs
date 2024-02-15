4b1bf6f46db061ca1e656218b09e73fc436ca548
Mahmoud Mohamed <matterm74m7@gmail.com>
2024-02-15 05:08:32 +0200

    Add files via upload

---
+++ Learning/C#/9_Inheritance/InheritanceFullExample/InheritanceFullExample/Developer.cs
@@ -0,0 +1,36 @@
+﻿namespace InheritanceFullExample
+{
+    public class Developer : Employee
+    {
+        private decimal commission = 0.3m;
+        private bool AccomplishedTasks { get; set; }
+
+        public Developer(int id, string name, decimal loggedHours, decimal wage, bool accomplishedTasks)
+            : base(id, name, loggedHours, wage) 
+        {
+            AccomplishedTasks = accomplishedTasks;
+        }
+        public override decimal CalculateSalary()
+        {
+            // get the  basic Salary
+            decimal basicSalary = base.CalculateSalary();
+
+            return basicSalary + CalculateBouns();
+        }
+
+        private decimal CalculateBouns()
+        {
+            return AccomplishedTasks ? commission * base.CalculateSalary() : 0;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() +
+                $"\nTask Compleated: {(AccomplishedTasks ? "Yes" : "No")}" +
+                $"\nBouns: {this.CalculateBouns():C}" +
+                $"\nNet Salary: {this.CalculateSalary():C}";
+        }
+
+
+    }
+}
---
+++ Learning/C#/9_Inheritance/InheritanceFullExample/InheritanceFullExample/Program.cs
@@ -0,0 +1,45 @@
+﻿/*
+Requirements: App to calculates the monthly salary slip
+
+Minimum Hours required: 176 hours (8 * 22)
+
+Basic salary = 176 * Wage
+
+Overtime = (additional hours * 1.25 * hourly cost) / Types of Employees
+
+Directors: 
+    manager's allowance (5% of total salary)
+Maintenance:
+    hardship allowance ($100 / month)
+sales:
+    commission percentage on volume of sale
+
+Programmers:
+    bonus 3% of his total salary if all assigned task were accomplished
+*/
+
+using System;
+
+namespace InheritanceFullExample
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var manger = new Manger(1, "Mahmoud M.", 180, 20);
+            Console.WriteLine(manger.ToString());
+
+            var maintanence = new Maintanence(2, "Ali Mohamed", 190, 10);
+            Console.WriteLine(maintanence.ToString());
+
+
+            var sales = new Sales(3, "Mona Gamal", 200, 11, 10000m, 0.10m);
+            Console.WriteLine(sales.ToString());
+
+            var developer = new Developer(id: 4, name: "Mahmoud Mohamed", 194, 17, true);
+
+            Console.WriteLine(developer.ToString());
+        }
+    }
+}
