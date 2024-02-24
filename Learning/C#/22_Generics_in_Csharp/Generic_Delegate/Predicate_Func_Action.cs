e23b799cc70669534a426a95394be5e0b9140630
Mahmoud Mohamed <matterm74m7@gmail.com>
2024-02-24 04:58:00 +0200

    Update Predicate_Func_Action.cs

--- Learning/C#/22_Generics_in_Csharp/Generic_Delegate/Predicate_Func_Action.cs
+++ Learning/C#/22_Generics_in_Csharp/Generic_Delegate/Predicate_Func_Action.cs
@@ -1,1 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Channels;
 
+namespace Predicate_Func_Action
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Action<string> action = Print;
+            action("Mahmoud");
+
+            Func<int, int, int> func = Add;
+            Console.WriteLine(func(1, 2));
+
+            Predicate<int> predicate = IsEven;
+            Console.WriteLine(predicate(1));
+        }
+
+        public static void Print(string value) => Console.WriteLine(value);
+        public static int Add(int n1, int n2) => n1 + n2;
+        public static bool IsEven(int value) => (value & 1) == 0;
+        
+    }
+}
