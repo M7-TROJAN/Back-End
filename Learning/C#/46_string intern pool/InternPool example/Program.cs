ee243642f5eda2bfcfb140d08a90c4d89fc4d9b6
Mahmoud Mohamed <matterm74m7@gmail.com>
2024-06-28 08:23:13 +0300

    Add files via upload

---
+++ Learning/C#/46_string intern pool/InternPool example/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace Mah_Mattar
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            // RunStringLiteralVsObject();
+            // RunStringLiteralKey();
+            // RunStringLiteralWithStringObjectComparison();
+            RunStringIntern();
+
+            Console.ReadKey();
+
+        }
+
+        public static void RunStringLiteralVsObject()
+        {
+            string s1 = "Mahmoud"; // string literal
+            string s2 = new string("Mahmoud"); // string object
+
+
+            Console.WriteLine(s1 == s2);                   // true same content (string override equality value based)
+            Console.WriteLine(s1.Equals(s2));              // true same content
+
+            Console.WriteLine((Object)s1 == (Object)s2);   // false two different reference
+            Console.WriteLine(ReferenceEquals(s1, s2));    // false two different reference
+
+        }
+
+        public static void RunStringLiteralKey()
+        {
+            string s1 = "Mahmoud"; // string literal
+            string s2 = "aspnet"; // string literal 
+            string s3 = "Mah" + "Moud"; // string literal
+
+
+
+            Console.WriteLine(s1 == s2);                   // false  different content
+            Console.WriteLine(ReferenceEquals(s1, s2));    // false two different reference
+
+
+            Console.WriteLine(s1 == s3);   // true same content
+            Console.WriteLine(ReferenceEquals(s1, s3));   // true same reference
+        }
+
+        public static void RunStringLiteralWithStringObjectComparison()
+        {
+            string s1 = "Mahmoud"; // string literal
+                                     //string s11 = "metigator";
+            string s2 = new string("Mahmoud");                  // string object
+
+            Console.WriteLine(s1 == s2);                   // true, same content
+
+            Console.WriteLine(ReferenceEquals(s1, s2));   // false different reference
+
+        }
+
+        public static void RunStringIntern()
+        {
+            string s1 = "Mahmoud";                       // string literal
+
+            string s2 = new string("Mahmoud");   // string object
+
+            string s3 = String.Intern(s2);                  // string literal
+
+            Console.WriteLine(s1 == s2);                   // true, same content 
+            Console.WriteLine(ReferenceEquals(s1, s2));   // false different reference
+
+            Console.WriteLine(s2 == s3);                   // true, same content 
+            Console.WriteLine(ReferenceEquals(s2, s3));   // false different reference
+
+            Console.WriteLine(s1 == s3);                   // true, same content 
+            Console.WriteLine(ReferenceEquals(s1, s3));   // true, same reference
+
+
+            string str = "xyz";
+
+            Console.WriteLine(str);
+
+        }
+    }
+}
