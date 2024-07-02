3bd1aa4450a666331a2298827f38776838e4f2d7
Mahmoud Mohamed <matterm74m7@gmail.com>
2024-07-02 12:47:59 +0300

    Delete Learning/Language Integrated Query - LINQ/01_Pure Vs. Impure Function/ImparativeVsDeclarative directory

--- Learning/Language Integrated Query - LINQ/01_Pure Vs. Impure Function/ImparativeVsDeclarative/Person.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ImparativeVsDeclarative
-{
-    class Person
-    {
-        public string Name { get; set; }
-        public int Age { get; set; }
-        public string Telephone  { get; set; }
-    }
-}
